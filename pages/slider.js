f3c0b668a9430f6776428d36c8b5ea2870881a18
Unyime Emmanuel <unyimeemmanuel012@gmail.com>
2022-12-11 12:31:05 +0100

    Updates in coherence with article

--- pages/slider.js
+++ pages/slider.js
@@ -12,47 +12,63 @@ const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
   ssr: false,
 });
 
+const Responsive = {
+  0: {
+    items: 1.5,
+    margin: 5
+  },
+  768: {
+    items: 2.5,
+    margin: 10
+  },
+  1024: {
+    items: 3.5,
+    margin: 20
+  }
+}
+
 export default function Slider() {
   return (
-    <div>
-      <OwlCarousel>
-        <div className="item">
-          <h4>1</h4>
-        </div>
-        <div className="item">
-          <h4>2</h4>
-        </div>
-        <div className="item">
-          <h4>3</h4>
-        </div>
-        <div className="item">
-          <h4>4</h4>
-        </div>
-        <div className="item">
-          <h4>5</h4>
-        </div>
-        <div className="item">
-          <h4>6</h4>
-        </div>
-        <div className="item">
-          <h4>7</h4>
-        </div>
-        <div className="item">
-          <h4>8</h4>
-        </div>
-        <div className="item">
-          <h4>9</h4>
-        </div>
-        <div className="item">
-          <h4>10</h4>
-        </div>
-        <div className="item">
-          <h4>11</h4>
-        </div>
-        <div className="item">
-          <h4>12</h4>
-        </div>
-      </OwlCarousel>
-    </div>
+
+    <OwlCarousel
+      responsive={Responsive}
+    >
+      <div className="item">
+        <h4>1</h4>
+      </div>
+      <div className="item">
+        <h4>2</h4>
+      </div>
+      <div className="item">
+        <h4>3</h4>
+      </div>
+      <div className="item">
+        <h4>4</h4>
+      </div>
+      <div className="item">
+        <h4>5</h4>
+      </div>
+      <div className="item">
+        <h4>6</h4>
+      </div>
+      <div className="item">
+        <h4>7</h4>
+      </div>
+      <div className="item">
+        <h4>8</h4>
+      </div>
+      <div className="item">
+        <h4>9</h4>
+      </div>
+      <div className="item">
+        <h4>10</h4>
+      </div>
+      <div className="item">
+        <h4>11</h4>
+      </div>
+      <div className="item">
+        <h4>12</h4>
+      </div>
+    </OwlCarousel>
   );
 }
